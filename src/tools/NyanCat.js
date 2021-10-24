161c49bd4490312b2a2fa7e0e398fdd00a574538
aleksns <75623459+aleksns@users.noreply.github.com>
2021-10-24 18:47:51 +0300

    edited typo in png file

--- src/tools/NyanCat.js
+++ src/tools/NyanCat.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../App.css";
 
 export default function NyanCat(context2Ref) {
-  const cursorNyanCat = `url(cursor-nyanCat.png) 34 34, auto`;
+  const cursorNyanCat = `url(cursor-nyancat.png) 34 34, auto`;
   const [isNyanCat, setIsNyanCat] = useState(false);
 
   const setNyanCatValues = () => {

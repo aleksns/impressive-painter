03d9d7bd1b6f9690f3193b7bbda85b2f0ea49b81
aleksns <75623459+aleksns@users.noreply.github.com>
2021-10-24 17:27:43 +0300

    gh pages commit

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -10,7 +10,7 @@ export default function Canvas(props) {
     handleMouseUp,
     handleMouseMove,
   } = props;
-
+ 
   return (
     <div className="canvas-box">
       <div className="canvas-background"></div>

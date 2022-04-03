f574ce6ca03b72a3bb1e7ef9f375fcff04afa3d7
Aleksns <aleksnmsg@gmail.com>
2022-04-04 00:06:22 +0300

    Updates

--- src/components/LogoNyanCat.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../App.css";
-import logoNyanCat from "../images/logo-nyancat.png";
-
-export default function LogoNyanCat() {
-    return <img src={logoNyanCat} className="logo-nyancat"></img>;
-  }

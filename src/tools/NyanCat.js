9be585c0bb5b4e9193e3c8cc4a7d7e98e6246f23
Aleksns <aleksnmsg@gmail.com>
2022-04-04 00:18:35 +0300

    changed cursor pic location

--- src/tools/NyanCat.js
+++ src/tools/NyanCat.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "../App.css";
+import cursornyancat from "../images/cursornyancat.png"
 
 export default function NyanCat(context2Ref) {
-  const cursorNyanCat = `url(cursor-nyanCat.png) 34 34, auto`;
+  const cursorNyanCat = `url(${cursornyancat}) 34 34, auto`;
   const [isNyanCat, setIsNyanCat] = useState(false);
 
   const setNyanCatValues = () => {
